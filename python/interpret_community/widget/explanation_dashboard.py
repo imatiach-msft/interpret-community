18045ae611a34673af1a9f975a58b09840848b59
Ilya Matiach <ilmat@microsoft.com>
2020-06-15 17:36:54 -0400

    fixup

--- python/interpret_community/widget/explanation_dashboard.py
+++ python/interpret_community/widget/explanation_dashboard.py
@@ -83,8 +83,9 @@ class ExplanationDashboard:
             # Support credentials for notebook VM scenario
             instance_name = nbvm["instance"]
             domain_suffix = nbvm["domainsuffix"]
-            nbvm_origin = "https://{}*.{}".format(instance_name, domain_suffix)
-            cors = CORS(app, resources={r'/*': {'origins': [nbvm_origin]}}, supports_credentials=True)
+            nbvm_origin1 = "https://{}.{}".format(instance_name, domain_suffix)
+            nbvm_origin2 = "https://{}-5000.{}".format(instance_name, domain_suffix)
+            cors = CORS(app, resources={r'/*': {'origins': [nbvm_origin1, nbvm_origin2]}}, supports_credentials=True)
             # cors = CORS(app, resources={r'/*': {'origins': '*'}})
         app.config['CORS_HEADERS'] = 'Content-Type'
         logging.getLogger('flask_cors').level = logging.DEBUG
