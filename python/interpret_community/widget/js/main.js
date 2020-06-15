5e8ea80e47daf7c832529ae51269674fdc31e9a6
Ilya Matiach <ilmat@microsoft.com>
2020-06-15 11:54:25 -0400

    fixup\

--- python/interpret_community/widget/js/main.js
+++ python/interpret_community/widget/js/main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 import { ExplanationDashboard } from 'interpret-dashboard';
 
@@ -21,7 +22,35 @@ const RenderDashboard = (divId, data) => {
         'Content-Type': 'application/json',
         // 'X-XSRF-TOKEN': getCookieValue('XSRF-TOKEN')
     }
-    fetch('https://nbvm-5000.eastus2.instances.azureml.net/', {headers: headers_data1, credentials: 'include', method: "get", mode: 'cors'}).then(resp => {
+    const instance = axios.create({headers: headers_data1, withCredentials: true, baseURL: 'https://nbvm-5000.eastus2.instances.azureml.net/'})
+    instance.get()
+    request = new Request('https://nbvm.eastus2.instances.azureml.net/', {headers: headers_data1, credentials: 'include', method: "get", mode: 'cors'}).headers
+    fetch(request).then(resp => {
+        if (resp.status == 302) {
+            let auth_cookie = resp.headers.get('Set-Cookie')
+            var headers_data_auth = {
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                'Content-Type': 'application/json',
+                'Cookie': auth_cookie
+            }
+            // do the login thing, setting Cookie from initial response
+            return fetch("auth.eastus2.instances.azureml.net/signin-oidc2", {headers: headers_data_auth, credentials: 'include', method: "post", mode: 'cors'}).then(resp => {
+                resp.headers.get('Set-Cookie')
+            })
+        } else if (resp.status >= 200 && resp.status < 300) {
+            fetch(data.predictionUrl, {headers: headers_data2, credentials: 'include', method: "post", mode: 'cors', body: JSON.stringify(postData)}).then(resp => {
+                if (resp.status >= 200 && resp.status < 300) {
+                  return resp.json()
+                }
+                return Promise.reject(new Error(resp.statusText))
+              }).then(json => {
+                if (json.error !== undefined) {
+                  throw new Error(json.error)
+                }
+                return Promise.resolve(json.data)
+              })
+        }
+
     // return fetch(data.predictionUrl, {method: "get", body: JSON.stringify(postData), headers: headers_data, mode: 'cors'}).then(resp => {
     return fetch(data.predictionUrl, {headers: headers_data2, credentials: 'include', method: "post", mode: 'cors', body: JSON.stringify(postData)}).then(resp => {
       if (resp.status >= 200 && resp.status < 300) {
