db5242bd25af58ecec9f8e87e2daf9ed180de002
Ilya Matiach <ilmat@microsoft.com>
2020-06-16 11:06:10 -0400

    fixup

--- python/interpret_community/widget/explanation_dashboard.py
+++ python/interpret_community/widget/explanation_dashboard.py
@@ -127,7 +127,7 @@ class ExplanationDashboard:
             nbvm_origin1 = "https://{}.{}".format(instance_name, domain_suffix)
             nbvm_origin2 = "https://{}-5000.{}".format(instance_name, domain_suffix)
             # cors = CORS(app, origins=[nbvm_origin1, nbvm_origin2], expose_headers=['POST', 'GET', 'OPTIONS'], supports_credentials=True, send_wildcard=True)
-            cross_origin(origins=[nbvm_origin_global, nbvm_origin2_global], allow_headers=['Content-Type','Authorization'], expose_headers=['POST', 'GET', 'OPTIONS'], supports_credentials=True, automatic_options=False, send_wildcard=True)(predict)
+            cross_origin(origins=[nbvm_origin_global, nbvm_origin2_global], allow_headers=['Content-Type','Authorization'], expose_headers=['Content-Type','Authorization'], supports_credentials=True, automatic_options=False, send_wildcard=True)(predict)
             # cors = CORS(app, resources={r'/*': {'origins': '*'}})
         app.config['CORS_HEADERS'] = 'Content-Type'
 
